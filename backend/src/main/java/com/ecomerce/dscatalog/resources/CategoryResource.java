108f6f62aa04d8c93557c7ea07997171ebda9316
Gabrielleao01 <gabriel.lindorio21@gmail.com>
2021-09-16 21:03:34 -0300

    DTO

--- backend/src/main/java/com/ecomerce/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/ecomerce/dscatalog/resources/CategoryResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ecomerce.dscatalog.entities.Category;
+import com.ecomerce.dscatalog.dto.CategoryDTO;
 import com.ecomerce.dscatalog.services.CategoryService;
 
 @RestController
@@ -20,11 +20,8 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-		/*List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Electronics"));*/
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll(){
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
