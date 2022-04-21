2543aea13b0cc4f977998c0bb336eb00fd106b01
Gabrielleao01 <gabriel.lindorio21@gmail.com>
2022-04-20 22:44:04 -0300

    frontweb created

--- backend/src/main/java/com/ecomerce/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/ecomerce/dscatalog/repositories/ProductRepository.java
@@ -1,11 +1,19 @@
 package com.ecomerce.dscatalog.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.ecomerce.dscatalog.entities.Category;
 import com.ecomerce.dscatalog.entities.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,	Long> {
 
+	@Query(value = "SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE "
+			+ "(:category IS NULL OR :category IN cats) AND "
+			+ "(LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')))")
+	Page<Product> find(Category category, String name, Pageable pageable);
 }
