c2654c21c72d9b82661e7fd5700a755610202c9f
Gabrielleao01 <gabriel.lindorio21@gmail.com>
2022-04-25 20:59:49 -0300

    Home styling

---
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+
+function ButtonIcon () {
+    return (
+    <>
+        <button className="btn btn-primary btn-icon">
+            <h6>INICIE AGORA SUA BUSCA</h6>
+        </button>
+    </>
+    )
+}
+
+export default ButtonIcon;
