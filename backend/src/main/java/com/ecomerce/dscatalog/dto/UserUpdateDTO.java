407d676157f1d18893a26d13feb78cad30a6e5fd
Gabrielleao01 <gabriel.lindorio21@gmail.com>
2022-06-01 20:46:05 -0300

    Fixing problems

--- backend/src/main/java/com/ecomerce/dscatalog/dto/UserUpdateDTO.java
+++
@@ -1,9 +0,0 @@
-package com.ecomerce.dscatalog.dto;
-
-import com.ecomerce.dscatalog.services.validation.UserUpdateValid;
-
-@UserUpdateValid
-public class UserUpdateDTO extends UserDTO {
-	private static final long serialVersionUID = 1L;
-	
-}
--- backend/src/main/java/com/ecomerce/dscatalog/services/exceptions/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.ecomerce.dscatalog.services.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-	
-	public ResourceNotFoundException(String msg) {
-		super(msg);
-	}	 
-}
