bb04b3f6b71e5a2fbe4f466fde21c6e1854e0b31
Gabrielleao01 <gabriel.lindorio21@gmail.com>
2022-06-13 20:16:58 -0300

    layout mobile

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,7 +1,34 @@
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
+import './style.css';
 
 function ProductDetails(){
     return (
-        <h1>Product Details</h1>
+        <div className="product-details-container">
+            <div className="base-card product-details-card">
+                <div className="goback-container">
+                    <ArrowIcon />
+                    <h2>VOLTAR</h2>
+                </div>
+                <div className="row">
+                    <div className="col-xl-6">
+                        <div className="img-container">
+                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="" />
+                        </div>
+                        <div className="name-price-container">
+                            <h1>Nome do produto</h1>
+                            <ProductPrice price={2345.90} />
+                        </div>
+                    </div>
+                    <div className="col-xl-6">
+                        <div className="description-container">
+                            <h2>Descrição do Produto</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum, ad.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
